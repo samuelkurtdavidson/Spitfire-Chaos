fa265844941ad4d846b4f1ee954750423739e5d3
Samuel Kurt Davidson <36154071+samuelkurtdavidson@users.noreply.github.com>
2018-03-14 19:53:06 +0100

    was missing a couple of pragma comments

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
 #pragma comment(lib, "SDL/libx86/SDL2main.lib")
+#pragma comment(lib, "SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib, "SDL_Mixer/libx86/SDL2_mixer.lib")
 
 int main(int argc, char* argv[]) {
 
